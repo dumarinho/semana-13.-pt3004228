a6d4e58f270399443d72718ee47f2f13d64bcfb6
dumarinho <38238581+dumarinho@users.noreply.github.com>
2023-11-16 21:36:31 -0300

    Update main.py
    teste

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import turma as t;
 alunos = []
 alunos.append(a.Aluno('Fabio', 'Teixeira', 11));
 alunos.append(a.Aluno('Fabiano', 'Teixeira', 7));
-alunos.append(a.Aluno('Melissa', 'Teixeira', 8));
+alunos.append(a.Aluno('Melissa', 'Teixeira', 9));
 alunos.append(a.Aluno('Rafael', 'Teixeira', 9));
 alunos.append(a.Aluno('Angela', 'Teixeira', 6));
 
